f76be41053d217c9894594ad7f8cc9fe9c5b5817
Igwar <43217217+Igwar@users.noreply.github.com>
2019-08-20 16:33:43 +0300

    Add files via upload

---
+++ JaggedArrayLib/JaggedArrayLib/Class1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrayLib
+{
+    public class JaggedArray
+    {
+        private int[][] arr;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="array"></param>
+        public JaggedArray(int[][] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+            this.arr = array;
+        }
+        /// <summary>
+        /// Property for array
+        /// </summary>
+        public int[][] Array { get { return arr; } }
+
+        /// <summary>
+        /// Swap method
+        /// </summary>
+        /// <param name="arr1"></param>
+        /// <param name="arr2"></param>
+        public void Swap(ref int[] arr1, ref int[] arr2)
+        {
+            int[] buff = arr1;
+            arr1 = arr2;
+            arr2 = buff;
+        }
+
+        /// <summary>
+        /// Method for searching min element
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public int Min(int[] array)
+        {
+            int min = int.MaxValue;
+            foreach (int item in array)
+            {
+                if (item < min)
+                {
+                    min = item;
+                }
+            }
+            return min;
+        }
+
+        /// <summary>
+        /// Method for searching max element
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public int Max(int[] array)
+        {
+            int max = int.MinValue;
+            foreach (int item in array)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+
+        /// <summary>
+        /// Method for summ
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public int Sum(int[] array)
+        {
+            int sum = 0;
+            foreach (int element in array)
+            {
+                sum += element;
+            }
+            return sum;
+        }
+
+    }
+
+
+
+
+    public static class Exten
+    {
+        /// <summary>
+        /// Method that sort by summ
+        /// </summary>
+        public static void SortBySumOfTheElements(this JaggedArray array)
+        {
+            for (int i = 0; i < array.Array.Length; i++)
+            {
+                for (int j = 0; j < array.Array[i].Length-1; j++)
+                {
+                    if (array.Sum(array.Array[i]) < array.Sum(array.Array[j]))
+                    {
+                        array.Swap(ref array.Array[i], ref array.Array[j]);
+                    }
+                }
+            }
+
+        }
+        /// <summary>
+        ///  Method that sort by max elements
+        /// </summary>
+        public static void SortByMaximumOfTheElements(this JaggedArray array)
+        {
+            for (int i = 0; i < array.Array.Length; i++)
+            {
+                for (int j = 0; j < array.Array[i].Length-1; j++)
+                {
+                    if (array.Max(array.Array[i]) < array.Max(array.Array[j]))
+                    {
+                        array.Swap(ref array.Array[i], ref array.Array[j]);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        ///  Method that sort by minimum elements
+        /// </summary>
+        public static void SortByMinimumOfTheElements(this JaggedArray array)
+        {
+            for (int i = 0; i < array.Array.Length; i++)
+            {
+                for (int j = 0; j < array.Array[i].Length-1; j++)
+                {
+                    if (array.Min(array.Array[i]) < array.Min(array.Array[j]))
+                    {
+                        array.Swap(ref array.Array[i], ref array.Array[j]);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ JaggedArrayLib/JaggedArrayLibTests/ExtenTests.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JaggedArrayLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrayLib.Tests
+{
+    [TestClass()]
+    public class ExtenTests
+    {
+        [TestMethod()]
+        public void SortBySumOfTheElementsTest()
+        {
+            int[][] b = new int[3][];
+
+            b[0] = new int[2] { 3, 5 };
+
+            b[1] = new int[4] { 8, 5, 3, 2 };
+            b[2] = new int[3] { 2, 9, 1 };
+            JaggedArray a = new JaggedArray(b);
+            int[][] f = new int[3][];
+
+            b[0] = new int[2] { 3, 5 };
+
+            b[1] = new int[3] { 2, 9, 1 };
+
+            b[2] = new int[4] { 8, 5, 3, 2 };
+            JaggedArray expect = new JaggedArray(f);
+            a.SortByMinimumOfTheElements();
+            for (int i = 0; i < expect.Array.Length; i++)
+            {
+                for (int j = 0; j < expect.Array[i].Length; j++)
+                {
+                    Assert.AreEqual(expect.Array[i][j], a.Array[i][j]);
+                }
+            }
+
+        }
+
+        [TestMethod()]
+        public void SortByMaximumOfTheElementsTest()
+        {
+            int[][] b = new int[3][];
+
+            b[0] = new int[2] { 3, 5 };
+
+            b[1] = new int[3] { 2, 9, 1 };
+
+            b[2] = new int[4] { 8, 5, 3, 2 };
+            JaggedArray a = new JaggedArray(b);
+            int[][] f = new int[3][];
+
+            b[0] = new int[2] { 3, 5 };
+           
+            b[1] = new int[4] { 8, 5, 3, 2 };
+
+            b[2] = new int[3] { 2, 9, 1 };
+            JaggedArray expect = new JaggedArray(f);
+            a.SortByMinimumOfTheElements();
+            for (int i = 0; i < expect.Array.Length; i++)
+            {
+                for (int j = 0; j < expect.Array[i].Length; j++)
+                {
+                    Assert.AreEqual(expect.Array[i][j], a.Array[i][j]);
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void SortByMinimumOfTheElementsTest()
+        {
+            int[][] b = new int[3][];
+
+            b[0] = new int[2] { 3, 5 };
+
+            b[1] = new int[3] { 2, 9, 1 };
+
+            b[2] = new int[4] { 8, 5, 3, 2 };
+            JaggedArray a = new JaggedArray(b);
+            int[][] f = new int[3][];
+
+            b[2] = new int[2] { 3, 5 };
+
+            b[0] = new int[3] { 2, 9, 1 };
+
+            b[1] = new int[4] { 8, 5, 3, 2 };
+            JaggedArray expect = new JaggedArray(f);
+            a.SortByMinimumOfTheElements();
+            for (int i = 0; i < expect.Array.Length; i++)
+            {
+                for (int j = 0; j < expect.Array[i].Length; j++)
+                {
+                    Assert.AreEqual(expect.Array[i][j], a.Array[i][j]);
+                }
+            }
+        }
+    }
+}
